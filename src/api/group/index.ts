8aa17c8d95d5a797072a05471756641f304daa00
wuzoo <wndyd9911@gmail.com>
2024-11-12 16:45:10 +0900

    feat: group api 작성

--- src/api/group/index.ts
+++ src/api/group/index.ts
@@ -1,5 +1,6 @@
-import { kyFileInstance } from "@/api";
+import { kyFileInstance, kyInstance } from "@/api";
 import type { groupSchema } from "@/api/group/schema";
+import type { GroupListResponse } from "@/api/group/type";
 import type { z } from "zod";
 
 export const postCreateGroup = async (
@@ -8,6 +9,54 @@ export const postCreateGroup = async (
   const response = await kyFileInstance.post<typeof groupSchema>("api/group", {
     json: formData,
   });
+
+  return response;
+};
+
+export const patchGroupInfo = async (formData: z.infer<typeof groupSchema>) => {
+  const response = await kyFileInstance.patch<typeof groupSchema>("api/group", {
+    json: formData,
+  });
+
+  return response;
+};
+
+export const getGroupList = async () => {
+  const response = await kyInstance
+    .get<GroupListResponse>("api/group/list")
+    .json();
+
+  return response;
+};
+
+export const getGroupInfo = async (groupId: number) => {
+  const response = await kyInstance
+    .get<GroupListResponse>(`api/group/group-info?groupId=${groupId}`)
+    .json();
+
+  return response;
+};
+
+export const getGroupInviteCode = async (groupId: number) => {
+  const response = await kyInstance
+    .get(`api/group/group-code?groupId=${groupId}`)
+    .json();
+
+  return response;
+};
+
+export const getGroupMemberList = async (groupId: number) => {
+  const response = await kyInstance
+    .get(`api/group/member-list?groupId=${groupId}`)
+    .json();
+
+  return response;
+};
+
+export const deleteGroup = async (groupId: number) => {
+  const response = await kyInstance.delete(
+    `api/group/leave?groupId=${groupId}`,
+  );
 
   return response;
 };
--- src/api/group/schema.ts
+++ src/api/group/schema.ts
@@ -9,7 +9,7 @@ export const groupSchema = z
       .max(15, { message: "최대 15자까지 입력할 수 있습니다." }),
     startDate: z.date(),
     endDate: z.date(),
-    desc: z.string(),
+    introduction: z.string(),
   })
   .refine((data) => data.endDate >= data.startDate, {
     message: "종료 일자는 시작 일자보다 나중이어야 합니다.",
