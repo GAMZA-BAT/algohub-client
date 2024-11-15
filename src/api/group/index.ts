0778a00605855f440d968bab36b162a2862cd23f
wuzoo <wndyd9911@gmail.com>
2024-11-16 00:44:15 +0900

    chore: pr 분리로 api 삭제

--- src/api/group/index.ts
+++ src/api/group/index.ts
@@ -1,23 +1,14 @@
 import { kyFileInstance, kyInstance } from "@/api";
-import type { groupSchema } from "@/api/group/schema";
-import type { GroupListResponse, GroupResponse } from "@/api/group/type";
-import type { z } from "zod";
+import type {
+  GroupListResponse,
+  GroupResponse,
+  MemberResponse,
+} from "@/api/group/type";
 
 export const postCreateGroup = async (formData: FormData) => {
   const response = await kyFileInstance.post("api/groups", {
     body: formData,
   });
-
-  return response;
-};
-
-export const patchGroupInfo = async (formData: z.infer<typeof groupSchema>) => {
-  const response = await kyFileInstance.patch<typeof groupSchema>(
-    "api/groups",
-    {
-      json: formData,
-    },
-  );
 
   return response;
 };
@@ -38,26 +29,10 @@ export const getGroupInfo = async (groupId: number) => {
   return response;
 };
 
-export const getGroupInviteCode = async (groupId: number) => {
-  const response = await kyInstance
-    .get(`api/groups/group-code?groupId=${groupId}`)
-    .json();
-
-  return response;
-};
-
 export const getGroupMemberList = async (groupId: number) => {
   const response = await kyInstance
-    .get(`api/groups/member-list?groupId=${groupId}`)
+    .get<MemberResponse[]>(`api/groups/${groupId}/members`)
     .json();
-
-  return response;
-};
-
-export const deleteGroup = async (groupId: number) => {
-  const response = await kyInstance.delete(
-    `api/groups/leave?groupId=${groupId}`,
-  );
 
   return response;
 };
--- src/app/group/[groupId]/setting/page.tsx
+++ src/app/group/[groupId]/setting/page.tsx
@@ -1,6 +1,5 @@
-import { getGroupInfo } from "@/api/group";
+import { getGroupInfo, getGroupMemberList } from "@/api/group";
 import Sidebar from "@/common/component/Sidebar";
-import { tmpMemberListData } from "@/shared/util/example";
 import { sidebarWrapper } from "@/styles/shared.css";
 import MemberList from "@/view/group/setting/MemberList";
 import SettingSidebar from "@/view/group/setting/SettingSidebar";
@@ -8,14 +7,17 @@ import SettingSidebar from "@/view/group/setting/SettingSidebar";
 const GroupSettingPage = async ({
   params,
 }: { params: { groupId: string } }) => {
-  const groupInfo = await getGroupInfo(+params.groupId);
+  const groupData = getGroupInfo(+params.groupId);
+  const memberData = getGroupMemberList(+params.groupId);
+
+  const [groupInfo, memberInfo] = await Promise.all([groupData, memberData]);
 
   return (
     <main className={sidebarWrapper}>
       <Sidebar>
         <SettingSidebar info={groupInfo} />
       </Sidebar>
-      <MemberList memberListData={tmpMemberListData} />
+      <MemberList memberListData={memberInfo} />
     </main>
   );
 };
