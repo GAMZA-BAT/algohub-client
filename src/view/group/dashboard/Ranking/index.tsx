723f8eebe6b6cfdd2b87b9754f2c59aec60ddb5a
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-11-15 16:57:04 +0900

    feat: top ranking 추출

--- src/view/group/dashboard/Ranking/index.tsx
+++ src/view/group/dashboard/Ranking/index.tsx
@@ -1,8 +1,9 @@
+import type { RankingResponse } from "@/api/groups/type";
 import TabGroup from "@/common/component/Tab";
 import AllRanking from "@/view/group/dashboard/Ranking/AllRanking";
 import TopRanking from "@/view/group/dashboard/Ranking/TopRanking";
 
-const Ranking = () => {
+const Ranking = ({ rankingData }: { rankingData: RankingResponse[] }) => {
   return (
     <TabGroup.Tabs tag="section" variant="secondary">
       <TabGroup.TabList>
@@ -10,7 +11,7 @@ const Ranking = () => {
         <TabGroup.Tab tabId="2">전체</TabGroup.Tab>
       </TabGroup.TabList>
       <TabGroup.TabPanels>
-        <TopRanking />
+        <TopRanking topRankingData={rankingData.slice(0, 3)} />
         <AllRanking />
       </TabGroup.TabPanels>
     </TabGroup.Tabs>
