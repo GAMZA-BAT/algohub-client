deb3f3afe4caec7810b91d0e68b2a021a6ac1ea8
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-11-16 17:15:50 +0900

    feat: AllRanking Diff 연결

--- src/view/group/dashboard/Ranking/RankList.tsx
+++ src/view/group/dashboard/Ranking/RankList.tsx
@@ -13,7 +13,19 @@ import {
 import Image from "next/image";
 
 const RankList = ({ info }: { info: RankingResponse }) => {
-  const { profileImage, rank, solvedCount, userNickname } = info;
+  const { profileImage, rank, solvedCount, userNickname, rankDiff } = info;
+
+  const change = () => {
+    if (rankDiff === "-") return "unChanged";
+    switch (rankDiff[0]) {
+      case "+":
+        return "up";
+      case "-":
+        return "down";
+    }
+    return "unChanged";
+  };
+
   return (
     <li className={listWrapper}>
       <Image
@@ -30,7 +42,7 @@ const RankList = ({ info }: { info: RankingResponse }) => {
           </div>
           <p className={solvedTextStyle}>{`${solvedCount} solved`}</p>
         </div>
-        <p className={changeStyle({ change: "up" })}>+2</p>
+        <p className={changeStyle({ change: change() })}>{rankDiff}</p>
       </div>
     </li>
   );
