e769dd09ab5b02b43eaf3018d1a83f2c8ea99cf2
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-11-21 02:35:17 +0900

    chore: svg명 변경된 거 revert

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,7 +5,7 @@ export const kyInstance = ky.create({
   headers: {
     "Content-Type": "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3MzE5OTUxNTJ9.k3pW8j3rHEqkrbaiFUJI7igCkYd9V6_NiQwvkR1bxuM",
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGVtYWlsLmNvbSIsImF1dGgiOiJST0xFX1VTRVIiLCJleHAiOjE3MzIyMDA4NjN9.oDMOpF9JtmBO01UHRzSu58s-bveE5Ou_jisDq9Eah5Q",
   },
 });
 
