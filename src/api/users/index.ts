e6017a264756c97103374a7e099ec8bdbe29ff9d
wuzoo <wndyd9911@gmail.com>
2024-11-27 19:02:41 +0900

    feat: 유효성 검증, signup api 작성

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,4 +1,4 @@
-import { kyInstance, kyPublicInstance } from "@/api";
+import { kyFileInstance, kyInstance, kyPublicInstance } from "@/api";
 import type { GroupListResponse } from "@/api/groups/type";
 import type { UserResponse } from "@/api/users/type";
 
@@ -20,6 +20,40 @@ export const getUsers = async (userNickname: string) => {
 
 export const getMe = async () => {
   const response = await kyInstance.get<UserResponse>("api/users/me").json();
+
+  return response;
+};
+
+export const checkNickname = async (nickname: string) => {
+  const response = await kyInstance.get(
+    `api/users/check-nickname?nickname=${nickname}`,
+  );
+
+  return response;
+};
+
+export const checkBojNickname = async (nickname: string) => {
+  const response = await kyInstance.get(
+    `api/users/check-baekjoon-nickname?bjNickname=${nickname}`,
+  );
+
+  return response;
+};
+
+export const checkEmail = async (email: string) => {
+  const response = await kyInstance.post("api/users/check-email", {
+    json: {
+      email,
+    },
+  });
+
+  return response;
+};
+
+export const postSignUp = async (formData: FormData) => {
+  const response = await kyFileInstance.post("api/users/sign-up", {
+    body: formData,
+  });
 
   return response;
 };
