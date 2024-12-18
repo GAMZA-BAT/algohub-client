3234620b57e92bf497879c91f21c840da5a0009c
Lee jin <83453646+j-nary@users.noreply.github.com>
2024-12-18 17:59:54 +0900

    feat: 알림 설정 목록 조회 API 연결

--- src/app/[user]/setting/query.ts
+++ src/app/[user]/setting/query.ts
@@ -1,5 +1,9 @@
 import { getGroupList, patchGroupVisibility } from "@/app/api/groups";
 import type { GroupResponse, GroupStatus } from "@/app/api/groups/type";
+import {
+  getNotificationsSettings,
+  patchNotificationsSettings,
+} from "@/app/api/notifications";
 import { useToast } from "@/common/hook/useToast";
 import { HTTP_ERROR_STATUS } from "@/shared/constant/api";
 import type { StudyListType } from "@/view/user/setting/StudyList/StudyListTable/type";
@@ -65,7 +69,48 @@ export const useVisibilityMutation = () => {
           break;
         }
         default: {
-          showToast("알 수 없는 에러입니다.", "error");
+          showToast("정상적으로 수정되지 않았습니다.", "error");
+        }
+      }
+    },
+  });
+};
+
+export const useNotificationSettingListQuery = () => {
+  return useSuspenseQuery({
+    queryKey: ["notifications", "setting"],
+    queryFn: getNotificationsSettings,
+  });
+};
+
+export const useNotificationSettingMutation = () => {
+  const queryClient = useQueryClient();
+
+  const { showToast } = useToast();
+
+  return useMutation({
+    mutationFn: patchNotificationsSettings,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["notifications", "setting"],
+      });
+    },
+    onError: (error: HTTPError) => {
+      if (!error.response) return;
+
+      const { status } = error.response;
+
+      switch (status) {
+        case HTTP_ERROR_STATUS.FORBIDDEN: {
+          showToast("참여하지 않은 그룹입니다.", "error");
+          break;
+        }
+        case HTTP_ERROR_STATUS.NOT_FOUND: {
+          showToast("존재하지 않는 그룹입니다.", "error");
+          break;
+        }
+        default: {
+          showToast("정상적으로 수정되지 않았습니다.", "error");
         }
       }
     },
