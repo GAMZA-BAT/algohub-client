a3dde116c57d7977c93b262d5eb0b08723035327
wuzoo <wndyd9911@gmail.com>
2025-01-18 21:42:28 +0900

    refactor: api 성공 후 나가기 클릭 시 onSuccess

--- src/view/group/index/WithdrawDialog/index.tsx
+++ src/view/group/index/WithdrawDialog/index.tsx
@@ -3,26 +3,23 @@ import PromptWithdraw from "@/view/group/index/WithdrawDialog/PromptWithdraw";
 import SuccessWithdraw from "@/view/group/index/WithdrawDialog/SuccessWithdraw";
 import { withdrawWrapper } from "@/view/group/index/WithdrawDialog/index.css";
 import { useWithdrawMutation } from "@/view/group/index/WithdrawDialog/query";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 type WithdrawDialogProps = {
   groupId: number;
+  onSuccess?: () => void;
 };
 
-const WithdrawDialog = ({ groupId }: WithdrawDialogProps) => {
+const WithdrawDialog = ({ groupId, onSuccess }: WithdrawDialogProps) => {
   const [isWithdrawn, setIsWithdrawn] = useState(false);
-  const userNickname = useSession().data?.user?.nickname;
-  const router = useRouter();
 
   const { mutate: withdrawMutate } = useWithdrawMutation();
 
   const handleBtnClick = () => {
     if (!isWithdrawn) {
-      withdrawMutate(groupId, {
-        onSuccess: () => router.push(`/${userNickname}`),
-      });
+      withdrawMutate(groupId);
+    } else if (isWithdrawn) {
+      onSuccess?.();
     }
 
     setIsWithdrawn(true);
