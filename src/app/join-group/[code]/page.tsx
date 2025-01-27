d2c820619dd2376c67cab82698b461f6633cb8a8
최주용 <121484561+wuzoo@users.noreply.github.com>
2025-01-27 15:56:34 +0900

    [ Fix ] 그룹 참여하기 라우팅 에러 해결 (#326)
    * fix: server action -> client fetching
    
    * refactor: restore server action
    
    * chore: delete unused hook

--- src/app/join-group/[code]/page.tsx
+++ src/app/join-group/[code]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import {
-  useGroupByCodeQuery,
-  useJoinGroupMutation,
-} from "@/app/join-group/[code]/query";
+import {} from "@/app/api/groups";
+import { joinGroupAction } from "@/app/join-group/[code]/action";
+import { useGroupByCodeQuery } from "@/app/join-group/[code]/query";
 import Button from "@/common/component/Button";
 import Modal from "@/common/component/Modal";
 import { HTTP_ERROR_STATUS } from "@/shared/constant/api";
@@ -16,25 +15,27 @@ import {
   errorWrapper,
   wrapper,
 } from "@/view/user/join-group/index.css";
+import { useMutation } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const JoinGroupPage = ({ params: { code } }: { params: { code: string } }) => {
   const [isJoinModalOpen, setIsJoinModalOpen] = useState(true);
+
   const { data: groupData } = useGroupByCodeQuery(code);
   const userNickname = useSession().data?.user?.nickname;
-  const { mutate: joinGroupMutate } = useJoinGroupMutation(groupData?.id || 0);
   const router = useRouter();
-
-  const handleAccept = () => {
-    joinGroupMutate(code, {
-      onError: (error: Error) => {
-        if (error.message.includes(`${HTTP_ERROR_STATUS.BAD_REQUEST}`))
-          setIsJoinModalOpen(false);
-      },
-    });
-  };
+  const { mutate: joinGroupMutate } = useMutation({
+    mutationFn: (code: string) => joinGroupAction(code),
+    onSuccess: () => {
+      router.push(`/group/${groupData?.id}`);
+    },
+    onError: (error: Error) => {
+      if (error.message.includes(`${HTTP_ERROR_STATUS.BAD_REQUEST}`))
+        setIsJoinModalOpen(false);
+    },
+  });
 
   const handleReject = () => {
     if (isJoinModalOpen) router.push(`/${userNickname}`);
@@ -52,10 +53,15 @@ const JoinGroupPage = ({ params: { code } }: { params: { code: string } }) => {
           <GroupInfoCard groupInfo={groupData} />
           <DecisionPrompt owner={groupData.ownerNickname} />
           <div className={btnWrapper}>
-            <Button size="medium" color="lg">
+            <Button type="button" size="medium" color="lg">
               거절하기
             </Button>
-            <Button size="medium" color="purple" onClick={handleAccept}>
+            <Button
+              type="button"
+              size="medium"
+              color="purple"
+              onClick={() => joinGroupMutate(code)}
+            >
               수락하기
             </Button>
           </div>
@@ -75,7 +81,12 @@ const JoinGroupPage = ({ params: { code } }: { params: { code: string } }) => {
           <p className={descErrorText}>
             이미 스터디원으로 참여하고 있어요. 해당 스터디홈으로 이동할게요.
           </p>
-          <Button size="medium" color="purple" onClick={handleMoveGroup}>
+          <Button
+            type="button"
+            size="medium"
+            color="purple"
+            onClick={handleMoveGroup}
+          >
             스터디홈으로 돌아가기
           </Button>
         </div>
