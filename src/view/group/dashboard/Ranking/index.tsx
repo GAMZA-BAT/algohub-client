15a605b7dbdcb0d2952751831ba6068d9cca5ad7
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-02-06 22:32:08 +0900

    refactor: 전체 랭킹 prefetch

--- src/view/group/dashboard/Ranking/index.tsx
+++ src/view/group/dashboard/Ranking/index.tsx
@@ -1,11 +1,24 @@
+"use client";
+import { getAllRanking } from "@/app/api/groups/ranking";
 import type { RankingContent } from "@/app/api/groups/type";
 import TabGroup from "@/common/component/Tab";
+import useGetGroupId from "@/shared/hook/useGetGroupId";
 import AllRanking from "@/view/group/dashboard/Ranking/AllRanking";
 import EmptyRanking from "@/view/group/dashboard/Ranking/EmptyRanking";
 import TopRanking from "@/view/group/dashboard/Ranking/TopRanking";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Ranking = ({ rankingData }: { rankingData: RankingContent[] }) => {
   const isValid = rankingData.length > 0;
+
+  const queryClient = useQueryClient();
+  const groupId = useGetGroupId();
+  const prefetchRanking = async () => {
+    await queryClient.prefetchQuery({
+      queryKey: ["ranking", +groupId],
+      queryFn: () => getAllRanking(+groupId, 0),
+    });
+  };
 
   return (
     <TabGroup.Tabs
@@ -17,7 +30,11 @@ const Ranking = ({ rankingData }: { rankingData: RankingContent[] }) => {
         <TabGroup.Tab tabId="1" indicatorId="ranking">
           TOP 랭킹
         </TabGroup.Tab>
-        <TabGroup.Tab tabId="2" indicatorId="ranking">
+        <TabGroup.Tab
+          tabId="2"
+          indicatorId="ranking"
+          onMouseEnter={prefetchRanking}
+        >
           전체
         </TabGroup.Tab>
       </TabGroup.TabList>
