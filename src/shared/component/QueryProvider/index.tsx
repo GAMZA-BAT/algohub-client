c4b9fa5c69c810f35fc51484ce5a9e66f7763ac6
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-02-09 22:41:19 +0900

    [ Fix ] prefetch 적용 (#340)
    * fix: prefetch 적용
    
    * feat: prefetchQueries 추가

--- src/shared/component/QueryProvider/index.tsx
+++ src/shared/component/QueryProvider/index.tsx
@@ -3,12 +3,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function QueryProvider({
+const createQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+const QueryProvider = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
-  const [client] = useState(new QueryClient());
+}) => {
+  const [client] = useState(createQueryClient());
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
-}
+};
+
+export default QueryProvider;
