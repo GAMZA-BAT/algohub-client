cdcb3d18b30cfc9826e2862ee8e5a0eab9ec6f3f
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-03-11 19:13:17 +0900

    chore: 안쓰는 파일 삭제

--- src/asset/svg/index.ts
+++ src/asset/svg/index.ts
@@ -95,4 +95,3 @@ export { default as IcnUnsolved } from "./icn_unsolved.svg";
 export { default as IcnUser } from "./icn_user.svg";
 export { default as IcnUser2 } from "./icn_user2.svg";
 export { default as IcnUser3 } from "./icn_user3.svg";
-export { default as IcnGitHub } from "./logo_GitHub.svg";
