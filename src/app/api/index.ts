1022b0f23acec7d82fd9790bf6e6c8ec66ab4181
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-03-16 23:13:51 +0900

    fix: PUBLIC_ 접두어 적용

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -42,7 +42,7 @@ const RETRY = 2;
 const prefixUrl =
   process.env.NEXT_PUBLIC_APP_ENV === "production"
     ? process.env.NEXT_PUBLIC_HOST
-    : process.env.NEXT_PRIVATE_HOST;
+    : process.env.NEXT_PUBLIC_RC_HOST;
 
 export const kyJsonInstance = ky.create({
   prefixUrl,
