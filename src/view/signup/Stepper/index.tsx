c6422aaa2f1f3cf62ea2ee454a43a3a14969f9e3
Lee jin <83453646+j-nary@users.noreply.github.com>
2025-03-17 20:21:03 +0900

    refactor: Stepper 리팩토링

--- src/view/signup/Stepper/index.tsx
+++ src/view/signup/Stepper/index.tsx
@@ -12,43 +12,45 @@ type StepperProps = {
   stepName: string[];
 };
 
-const Stepper = ({ curStep, stepName }: StepperProps) => {
-  const StateIcon = {
-    done: <IcnStepperDone width={20} height={20} aria-label={"done-step"} />,
-    current: (
-      <IcnStepperCurrent width={20} height={20} aria-label={"current-step"} />
-    ),
-    todo: <IcnStepperTodo width={20} height={20} aria-label={"todo-step"} />,
-  };
+const StateIcon = {
+  done: <IcnStepperDone width={20} height={20} aria-label={"done-step"} />,
+  current: (
+    <IcnStepperCurrent width={20} height={20} aria-label={"current-step"} />
+  ),
+  todo: <IcnStepperTodo width={20} height={20} aria-label={"todo-step"} />,
+};
 
+const Stepper = ({ curStep, stepName }: StepperProps) => {
   return (
     <div className={stepperWrapper}>
-      {stepName.map((name, idx) => (
-        <div style={{ display: "flex" }} key={idx}>
-          {idx !== 0 && <div className={stepperLine} />}
-          <section className={stepperSectionStyle}>
-            {
-              StateIcon[
-                idx < curStep ? "done" : idx === curStep ? "current" : "todo"
-              ]
-            }
-            <h2
-              className={`${stepperTextStyle.title} ${
-                stepperTextColor[idx === curStep ? "active" : "inactive"]
-              }`}
-            >
-              {name}
-            </h2>
-            <p
-              className={`${stepperTextStyle.desc} ${
-                stepperTextColor[idx === curStep ? "active" : "inactive"]
-              }`}
-            >
-              {idx === curStep ? "입력 중" : idx < curStep ? "완료" : "입력 전"}
-            </p>
-          </section>
-        </div>
-      ))}
+      {stepName.map((name, idx) => {
+        const isDone = idx < curStep;
+        const isCurrent = idx === curStep;
+        const color = isCurrent ? "active" : "inactive";
+
+        return (
+          <div style={{ display: "flex" }} key={idx}>
+            {idx !== 0 && <div className={stepperLine} />}
+            <section className={stepperSectionStyle}>
+              {StateIcon[isDone ? "done" : isCurrent ? "current" : "todo"]}
+              <h2
+                className={`${stepperTextStyle.title} ${
+                  stepperTextColor[color]
+                }`}
+              >
+                {name}
+              </h2>
+              <p
+                className={`${stepperTextStyle.desc} ${
+                  stepperTextColor[color]
+                }`}
+              >
+                {isCurrent ? "입력 중" : isDone ? "완료" : "입력 전"}
+              </p>
+            </section>
+          </div>
+        );
+      })}
     </div>
   );
 };
