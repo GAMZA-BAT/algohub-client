ad51d3b8beba709e54f5430e09201a8e53f96720
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-05-01 20:55:48 +0900

    feat: 프로필 이미지에서 호버로 나타나는 수정, 삭제버튼으로 교체

--- src/shared/component/EditAvatar/index.css.ts
+++ src/shared/component/EditAvatar/index.css.ts
@@ -1,3 +1,4 @@
+import { colorToRgba } from "@/common/util/string";
 import { theme } from "@/styles/themes.css";
 import { style } from "@vanilla-extract/css";
 
@@ -22,6 +23,42 @@ export const iconStyle = style({
   borderRadius: "50%",
   opacity: 0.9,
 
+  cursor: "pointer",
+});
+
+export const buttonWrapper = style({
+  position: "absolute",
+  inset: 0,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "0.6rem",
+
+  borderRadius: "50%",
+  backgroundColor: "rgba(0, 0, 0, 0.4)",
+  opacity: 0,
+  transition: "opacity 0.2s ease-in-out",
+
+  selectors: {
+    "&:hover, &:focus-within": {
+      opacity: 1,
+    },
+  },
+});
+
+export const actionButton = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  width: "3.3rem",
+  height: "2.1rem",
+
+  borderRadius: "1rem",
+  border: `1px solid ${colorToRgba("#ffffff", 0.5)}`,
+
+  backgroundColor: colorToRgba("#bcc0cd", 0.5),
+  color: theme.color.white,
   cursor: "pointer",
 });
 
