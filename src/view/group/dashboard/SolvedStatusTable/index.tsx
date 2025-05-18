db805bc3f87ab357522321a5075fe5e650a30cfc
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-05-18 13:11:07 +0900

    refactor: DataTable 적용

--- src/view/group/dashboard/SolvedStatusTable/index.tsx
+++ src/view/group/dashboard/SolvedStatusTable/index.tsx
@@ -1,30 +1,19 @@
 "use client";
-import { SolutionsCurrentStatusResponse } from "@/app/api/type";
-import SolvedStatusTableHeader from "@/view/group/dashboard/SolvedStatusTable/SolvedStatusTableHeader";
-import { tableWrapper } from "./index.css";
-import SolvedStatusTableList from "@/view/group/dashboard/SolvedStatusTable/SolvedStatusTableList";
+
+import type { SolutionsCurrentStatusResponse } from "@/app/api/type";
+import SolvedStatusTableContent from "./SolvedStatusTableContent";
+import { SolvedStatusTableProvider } from "./provider";
+
 type SolvedStatusTableProps = {
   data: SolutionsCurrentStatusResponse[];
 };
 
 const SolvedStatusTable = ({ data }: SolvedStatusTableProps) => {
-  console.info({ data });
+  console.log({ data });
   return (
-    <div className={tableWrapper}>
-      <table>
-        <thead>
-          <SolvedStatusTableHeader problemCount={data[0].problems.length} />
-        </thead>
-        <tbody>
-          {data.map((item) => (
-            <SolvedStatusTableList
-              key={item.rank}
-              solutionsCurrentStatus={item}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <SolvedStatusTableProvider value={data}>
+      <SolvedStatusTableContent />
+    </SolvedStatusTableProvider>
   );
 };
 
---
+++ src/view/group/dashboard/SolvedStatusTable/provider.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import type { SolutionsCurrentStatusResponse } from "@/app/api/type";
+import type { TableDataType } from "@/shared/type/table";
+import { type ReactNode, createContext, useContext } from "react";
+
+type SolvedStatusTableContextValue = {
+  row: SolutionsCurrentStatusResponse[];
+  col: TableDataType<SolutionsCurrentStatusResponse>[];
+};
+
+const SolvedStatusTableContext =
+  createContext<SolvedStatusTableContextValue | null>(null);
+
+export const useSolvedStatusTable = () => {
+  const context = useContext(SolvedStatusTableContext);
+
+  if (!context) {
+    throw new Error(
+      "useSolvedStatusTable must be used within SolvedStatusTableProvider",
+    );
+  }
+
+  return context;
+};
+
+type SolvedStatusTableProviderProps = {
+  children: ReactNode;
+  value: SolutionsCurrentStatusResponse[];
+};
+
+const baseColumns: TableDataType<SolutionsCurrentStatusResponse>[] = [
+  {
+    key: "rank",
+    Header: () => "랭킹",
+    Cell: (data) => data.rank,
+    width: 75,
+  },
+  {
+    key: "id",
+    Header: () => "아이디",
+    Cell: (data) => data.nickname,
+    width: 75,
+  },
+  {
+    key: "total",
+    Header: () => "총점",
+    Cell: (data) => `${data.totalSubmissionCount}/${data.totalPassedTime}`,
+    width: 200,
+  },
+];
+
+export const SolvedStatusTableProvider = ({
+  children,
+  value,
+}: SolvedStatusTableProviderProps) => {
+  const problemColumns: TableDataType<SolutionsCurrentStatusResponse>[] =
+    value[0].problems.map((_, index) => ({
+      key: `problem${index + 1}`,
+      Header: () => `문제${index + 1}`,
+      Cell: (data) =>
+        `${data.problems[index].submissionCount}/${data.problems[index].firstCorrectDuration}`,
+      width: 100,
+    }));
+
+  const col = [...baseColumns, ...problemColumns];
+
+  return (
+    <SolvedStatusTableContext.Provider value={{ col, row: value }}>
+      {children}
+    </SolvedStatusTableContext.Provider>
+  );
+};
