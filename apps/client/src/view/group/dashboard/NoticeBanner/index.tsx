56a0a800b3eee16d40eaaa9729745c6f3f2b98c8
j-nary <dlwls0921@naver.com>
2025-06-10 13:42:18 +0900

    chore: fix format

--- apps/client/src/view/group/dashboard/NoticeBanner/index.tsx
+++ apps/client/src/view/group/dashboard/NoticeBanner/index.tsx
@@ -20,49 +20,53 @@ const NoticeBanner = () => {
   const { content: noticeList } = useNoticesQuery({ groupId: +groupId });
 
   const recentNotice = noticeList?.length > 0 ? noticeList[0] : null;
-  console.info({recentNotice})
+  console.info({ recentNotice });
   return (
-      <section
-        role="status"
-        aria-live="polite"
-        className={bannerWrapper}
-        aria-label="공지사항"
-      >
-        <button
-          className={overlayStyle}
-          aria-label="공지 모달 열기"
-          onClick={() => router.push(`/group/${groupId}/notice`)}
-        />
-        <header className={headerWrapper}>
-          <div className={notifyWrapper}>
-            <IcnNotifications width={24} height={24} focusable="false" />
-            <p className={textStyle.notification}>공지</p>
-          </div>
-          {recentNotice ? (
-            <>
-              <Avatar size="mini" src={recentNotice.authorImage} alt="방장 프로필 사진" />
-              <h2 className={textStyle.category}>{recentNotice.category}</h2>
-            </>
-          ) : (
-            <p className={textStyle.time}>공지가 없습니다.</p>
-          )}
-        </header>
-        {recentNotice && (
-          <div className={contentWrapper}>
-            <p className={textStyle.bannerTitle}>{recentNotice.title}</p>
-            <time className={textStyle.time} dateTime={recentNotice.createdAt}>
-              {formatDistanceDate(recentNotice.createdAt)}
-            </time>
-            {
-              <IcnNew
-                width={13}
-                height={13}
-                style={{ minWidth: 13, opacity: recentNotice.isRead ? 0 : 1 }}
-              />
-            }
-          </div>
+    <section
+      role="status"
+      aria-live="polite"
+      className={bannerWrapper}
+      aria-label="공지사항"
+    >
+      <button
+        className={overlayStyle}
+        aria-label="공지 모달 열기"
+        onClick={() => router.push(`/group/${groupId}/notice`)}
+      />
+      <header className={headerWrapper}>
+        <div className={notifyWrapper}>
+          <IcnNotifications width={24} height={24} focusable="false" />
+          <p className={textStyle.notification}>공지</p>
+        </div>
+        {recentNotice ? (
+          <>
+            <Avatar
+              size="mini"
+              src={recentNotice.authorImage}
+              alt="방장 프로필 사진"
+            />
+            <h2 className={textStyle.category}>{recentNotice.category}</h2>
+          </>
+        ) : (
+          <p className={textStyle.time}>공지가 없습니다.</p>
         )}
-      </section>
+      </header>
+      {recentNotice && (
+        <div className={contentWrapper}>
+          <p className={textStyle.bannerTitle}>{recentNotice.title}</p>
+          <time className={textStyle.time} dateTime={recentNotice.createdAt}>
+            {formatDistanceDate(recentNotice.createdAt)}
+          </time>
+          {
+            <IcnNew
+              width={13}
+              height={13}
+              style={{ minWidth: 13, opacity: recentNotice.isRead ? 0 : 1 }}
+            />
+          }
+        </div>
+      )}
+    </section>
   );
 };
 
