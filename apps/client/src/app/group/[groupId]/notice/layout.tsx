820290949ffad2e20edb089072cd770dfc47df7c
j-nary <dlwls0921@naver.com>
2025-07-30 00:58:57 +0900

    feat: GroupDashboardLayout에도 반영

--- apps/client/src/app/group/[groupId]/notice/layout.tsx
+++ apps/client/src/app/group/[groupId]/notice/layout.tsx
@@ -1,11 +1,13 @@
 import { getGroupInfo, getGroupMemberList } from "@/app/api/groups";
 import { getTopRanking } from "@/app/api/groups/ranking";
+import { getSolutionsCurrentStatus } from "@/app/api/solutions";
 import { listSectionStyle } from "@/app/group/[groupId]/page.css";
 import Sidebar from "@/common/component/Sidebar";
 import { sidebarWrapper } from "@/styles/shared.css";
 import GroupSidebar from "@/view/group/dashboard/GroupSidebar";
 import NoticeBanner from "@/view/group/dashboard/NoticeBanner";
 import Ranking from "@/view/group/dashboard/Ranking";
+import SolvedStatusSection from "@/view/group/dashboard/SolvedStatusSection";
 import type { ReactNode } from "react";
 
 const GroupDashboardLayout = async ({
@@ -15,11 +17,13 @@ const GroupDashboardLayout = async ({
   const groupInfoData = getGroupInfo(+groupId);
   const rankingData = getTopRanking(+groupId);
   const memberData = getGroupMemberList(+groupId);
+  const solutionsCurrentStatusData = getSolutionsCurrentStatus(+groupId);
 
-  const [groupInfo, rankingInfo, memberInfo] = await Promise.all([
+  const [groupInfo, rankingInfo, memberInfo, solutionsCurrentStatusInfo] = await Promise.all([
     groupInfoData,
     rankingData,
     memberData,
+    solutionsCurrentStatusData,
   ]);
 
   return (
@@ -30,6 +34,9 @@ const GroupDashboardLayout = async ({
       <div className={listSectionStyle}>
         <NoticeBanner />
         <Ranking rankingData={rankingInfo} />
+        <SolvedStatusSection
+          solutionsCurrentStatusInfo={solutionsCurrentStatusInfo}
+        />
       </div>
       {children}
     </main>
