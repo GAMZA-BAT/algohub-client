d71adf538cb0ecf16dd22b4936f59a61353e5ab0
hwangjokim <hg7258@naver.com>
2025-08-03 01:15:07 +0900

    Fail check

--- apps/client/src/app/api/auth/callback/github/route.ts
+++ apps/client/src/app/api/auth/callback/github/route.ts
@@ -2,7 +2,7 @@ import { kyJsonInstance } from "@/app/api";
 import { getMyInfo } from "@/app/api/users";
 import { signIn } from "@/auth";
 import { type NextRequest, NextResponse } from "next/server";
-import type { tokenResponse } from "../../type";
+import  { tokenResponse } from "../../type";
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
@@ -11,18 +11,19 @@ export async function GET(req: NextRequest) {
   if (!code) {
     return NextResponse.json(
       { error: "Authorization code not found" },
-      { status: 400 },
+      { status: 400 
     );
   }
 
   const tokens = await kyJsonInstance
-    .post<tokenResponse>(`api/oauth/github/sign-in?code=${code}`)
-    .json();
+    .post<tokenResponse>(`api/oauth/github/sign-in?code=${code}`).json();
   const user = await getMyInfo(tokens.accessToken);
 
   await signIn("github-login", {
     ...tokens,
     ...user,
+
+
     redirectTo: `/${user?.nickname}`,
   });
 }
