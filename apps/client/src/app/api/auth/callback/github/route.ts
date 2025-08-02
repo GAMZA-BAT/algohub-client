5427d3a60598aed79aeed9179e7228d781cb8b64
hwangjokim <hg7258@naver.com>
2025-08-03 01:21:30 +0900

    Revert "Fail check"
    This reverts commit 0ae33f00d94dfb709853922354462241cd75dc01.

--- apps/client/src/app/api/auth/callback/github/route.ts
+++ apps/client/src/app/api/auth/callback/github/route.ts
@@ -2,7 +2,7 @@ import { kyJsonInstance } from "@/app/api";
 import { getMyInfo } from "@/app/api/users";
 import { signIn } from "@/auth";
 import { type NextRequest, NextResponse } from "next/server";
-import  { tokenResponse } from "../../type";
+import type { tokenResponse } from "../../type";
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
@@ -11,19 +11,18 @@ export async function GET(req: NextRequest) {
   if (!code) {
     return NextResponse.json(
       { error: "Authorization code not found" },
-      { status: 400 
+      { status: 400 },
     );
   }
 
   const tokens = await kyJsonInstance
-    .post<tokenResponse>(`api/oauth/github/sign-in?code=${code}`).json();
+    .post<tokenResponse>(`api/oauth/github/sign-in?code=${code}`)
+    .json();
   const user = await getMyInfo(tokens.accessToken);
 
   await signIn("github-login", {
     ...tokens,
     ...user,
-
-
     redirectTo: `/${user?.nickname}`,
   });
 }
