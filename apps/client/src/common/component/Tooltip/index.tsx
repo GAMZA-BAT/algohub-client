c83b6f29a4813a0bdbb500df1b24aac035a0edc2
Lee jin (nari) <83453646+j-nary@users.noreply.github.com>
2025-08-10 22:51:16 +0900

    [ Feat ] Tooltip 공통 컴포넌트 구현 (#434)
    * feat: Tooltip 구현
    
    * chore: CODEOWNERS 수정
    
    * chore: type import 분리
    
    * refactor: 코드리뷰 반영
    
    * refactor: onFocus에 useA11yHoverHandler 추가
    
    * chore: fix ci
    
    * refactor: button -> div 로 변경
    
    * chore: fix ci
    
    * chore: aria-describedby 수정
    
    * chore: 코드리뷰 반영
    
    * chore: 나머지 둘 처리
    
    * chore: fix ci
    
    * chore: fix format

---
+++ apps/client/src/common/component/Tooltip/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import useA11yHoverHandler from "@/shared/hook/useA11yHandler";
+import { useId } from "react";
+import type { HTMLAttributes, ReactNode } from "react";
+import {
+  tooltipWrapper,
+  triggerWrapper,
+  tooltipContainer,
+  tooltipArrow,
+} from "./index.css";
+
+type TooltipPosition =
+  | "top-center"
+  | "top-left"
+  | "top-right"
+  | "bottom-center"
+  | "bottom-left"
+  | "bottom-right"
+  | "left-center"
+  | "left-top"
+  | "left-bottom"
+  | "right-center"
+  | "right-top"
+  | "right-bottom";
+
+interface TooltipProps extends Omit<HTMLAttributes<HTMLDivElement>, "content"> {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  content: ReactNode;
+  placement?: TooltipPosition;
+}
+
+const Tooltip = ({
+  open,
+  onOpenChange,
+  children,
+  content,
+  placement = "bottom-left",
+}: TooltipProps) => {
+  const tooltipId = useId();
+  const { onFocus, onBlur } = useA11yHoverHandler();
+
+  const handleMouseEnter = () => {
+    onOpenChange(true);
+  };
+  const handleMouseLeave = () => {
+    onOpenChange(false);
+  };
+
+  return (
+    <div className={tooltipWrapper}>
+      <div
+        className={triggerWrapper}
+        tabIndex={0}
+        role="button"
+        aria-describedby={`${tooltipId}-tooltip`}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {children}
+      </div>
+      {open && (
+        <div
+          className={tooltipContainer({ placement })}
+          role="tooltip"
+          id={`${tooltipId}-tooltip`}
+        >
+          <div className={tooltipArrow({ placement })} />
+          {content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
