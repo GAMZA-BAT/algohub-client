ba3b89916b79277e1a951518098beb46d309038a
rtttr1 <bobo3766@naver.com>
2025-08-17 21:30:52 +0900

    feat: 검색 input 구현

---
+++ apps/client/src/app/[user]/components/SearchStudyInput/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  inputStyle,
+  inputWrapper,
+} from "@/app/[user]/components/SearchStudyInput/index.css";
+import { IcnSearch } from "@/asset/svg";
+import { type KeyboardEvent, useRef } from "react";
+
+const SearchStudyInput = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const searchStudy = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      //TODO: 검색 api 연결 (검색 페이지로 네비게이트)
+    }
+  };
+
+  return (
+    <label className={inputWrapper} aria-label="스터디 검색 input">
+      <IcnSearch width={16} height={16} />
+      <input
+        ref={inputRef}
+        type="search"
+        placeholder="스터디, 키워드로 검색하기"
+        onKeyDown={searchStudy}
+        className={inputStyle}
+      />
+    </label>
+  );
+};
+
+export default SearchStudyInput;
