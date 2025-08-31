41cb14052768e7cef3a6c9cde6a862379f8752bb
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-08-31 12:13:53 +0900

    refactor: count 컴포넌트 분리

--- apps/client/src/app/[user]/components/LeftSidebar/index.css.ts
+++ apps/client/src/app/[user]/components/LeftSidebar/index.css.ts
@@ -27,22 +27,6 @@ export const titleStyle = style({
   color: theme.color.lg2,
 });
 
-export const countWrapper = style({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-
-  width: "2rem",
-  height: "2rem",
-  borderRadius: "5rem",
-  backgroundColor: theme.color.mg5,
-});
-
-export const countTextStyle = style({
-  ...theme.font.Caption3_M_12,
-  color: theme.color.mg2,
-});
-
 export const chipContainerStyle = style({
   display: "flex",
   flexDirection: "row",
