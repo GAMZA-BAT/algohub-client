5ce0fb1b8ee2965c9165f78f9fc5eb1995f28af9
j-nary <dlwls0921@naver.com>
2025-09-07 21:17:03 +0900

    feat: 틀린 문제 리스트 아이템 구현

---
+++ apps/client/src/app/[user]/edge-case/components/WrongSolvedList/WrongSolvedItem.tsx
@@ -0,0 +1,39 @@
+import {
+  solvedResultTagStyle,
+  solvedResultTagTextStyle,
+  wrongSolvedItemWrapper,
+  wrongSolvedMetaStyle,
+  wrongSolvedTitleContainer,
+  wrongSolvedTitleStyle,
+  wrongSolvedTitleWrapper,
+} from "@/app/[user]/edge-case/components/WrongSolvedList/index.css";
+import { IcnBtnArrowRight } from "@/asset/svg";
+import Avatar from "@/common/component/Avatar";
+import { getTierImage } from "@/shared/util/img";
+import Link from "next/link";
+
+const WrongSolvedItem = () => {
+  const TierIcon = getTierImage(1);
+
+  return (
+    <Link href={`/problem/${1166}`}>
+      <li className={wrongSolvedItemWrapper}>
+        <div className={wrongSolvedMetaStyle}>
+          <Avatar size="xMini" alt="group image" />
+          <div className={wrongSolvedTitleWrapper}>
+            <div className={wrongSolvedTitleContainer}>
+              <TierIcon width={16} height={16} />
+              <h3 className={wrongSolvedTitleStyle}>1166번: 선물</h3>
+            </div>
+            <div className={solvedResultTagStyle}>
+              <span className={solvedResultTagTextStyle}>시간 초과</span>
+            </div>
+          </div>
+        </div>
+        <IcnBtnArrowRight width={12} height={12} />
+      </li>
+    </Link>
+  );
+};
+
+export default WrongSolvedItem;
