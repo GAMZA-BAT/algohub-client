1e9e9cf07dae581f78969908a59e1716cd5606c1
rtttr1 <bobo3766@naver.com>
2025-09-21 21:44:43 +0900

    feat: 스터디 검색 api 연결

--- apps/client/src/app/[user]/components/RecommendSection/index.tsx
+++ apps/client/src/app/[user]/components/RecommendSection/index.tsx
@@ -1,19 +1,40 @@
 "use client";
 
 import SearchStudyInput from "@/app/[user]/components/SearchStudyInput";
+import { useSearchStudyQueryObject } from "@/app/api/groups/query";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
 import RecommendCard from "./RecommendCard";
-import { recommendHeaderWrapper, recommendSectionWrapper, recommendStudyTitle } from "./index.css";
+import {
+  recommendHeaderWrapper,
+  recommendSectionWrapper,
+  recommendStudyTitle,
+} from "./index.css";
 
 const RecommendStudySection = () => {
+  const searchParam = useSearchParams();
+  const searchPattern = searchParam.get("search") || "";
+
+  const { data: studyList } = useQuery(
+    useSearchStudyQueryObject({ searchPattern }),
+  );
+
   return (
-    <section className={recommendSectionWrapper} aria-labelledby="recommend-title">
+    <section
+      className={recommendSectionWrapper}
+      aria-labelledby="recommend-title"
+    >
       <div className={recommendHeaderWrapper}>
         <h2 id="recommend-title" className={recommendStudyTitle}>
           추천 스터디
         </h2>
         <SearchStudyInput />
       </div>
-      <RecommendCard />
+      {studyList?.content ? (
+        studyList.content.map((study) => <RecommendCard key={study.id} />)
+      ) : (
+        <RecommendCard />
+      )}
     </section>
   );
 };
