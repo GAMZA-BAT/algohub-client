b9d7f416562f1f2a5cd5eccf3de1ef31055d2d74
j-nary <dlwls0921@naver.com>
2025-09-28 20:25:25 +0900

    refactor: useClipboard 일반화

--- apps/client/src/shared/hook/useClipboard.ts
+++ apps/client/src/shared/hook/useClipboard.ts
@@ -1,15 +1,12 @@
 import { useToast } from "@/common/hook/useToast";
-import { APP_URL } from "@/shared/constant/url";
 import { useState } from "react";
 
-export const useClipboard = (ownerNickname: string, groupName: string) => {
+export const useClipboard = () => {
   const { showToast } = useToast();
   const [isCopied, setIsCopied] = useState(false);
 
-  const copy = async (code: string) => {
-    const inviteWriting = `[AlgoHub 알림]\n\n'${ownerNickname}'님께서 회원님을 ‘${groupName}’ 그룹에 초대하셨습니다!\n\n아래 링크를 클릭하셔서 초대를 수락해 주세요!\n함께 도전하며 발전하는 시간을 보내시길 기대합니다.\n\n지금 바로 참여하세요:\n${APP_URL}/join-group/${code}`;
-
-    await navigator.clipboard.writeText(inviteWriting).then(() => {
+  const copy = async (text: string) => {
+    await navigator.clipboard.writeText(text).then(() => {
       showToast("성공적으로 복사되었습니다.", "success");
       setIsCopied(true);
     });
