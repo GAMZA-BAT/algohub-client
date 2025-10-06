22c382abd099ebf7406745a88a6bcb39ad041a45
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-10-06 18:49:13 +0900

    feat: user 페이지 api 적용

--- apps/client/src/app/[user]/components/RecommendSection/index.tsx
+++ apps/client/src/app/[user]/components/RecommendSection/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import RecommendList from "@/app/[user]/components/RecommendSection/RecommendList";
+import { useJoinRecommendMutation } from "@/app/api/groups/mutation";
 import { useSearchStudyQueryObject } from "@/app/api/groups/query";
-import type { GroupResponse } from "@/app/api/groups/type";
+import { useRecommendStudyQueryObject } from "@/app/api/users/query";
 import Spinner from "@/common/component/Spinner";
 import { useBooleanState } from "@/common/hook/useBooleanState";
 import GroupActionModal from "@/shared/component/GroupActionModal";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import CardButton from "../CardButton";
@@ -20,6 +21,8 @@ import {
   searchedStudyCountStyle,
 } from "./index.css";
 
+const ROTATION_INTERVAL_MS = 5000;
+
 const RecommendStudySection = () => {
   const searchParam = useSearchParams();
   const searchPattern = searchParam.get("search") || "";
@@ -27,32 +30,32 @@ const RecommendStudySection = () => {
   const { data: studyList, isFetching } = useQuery(
     useSearchStudyQueryObject({ searchPattern }),
   );
+  const { data: groupInfos } = useSuspenseQuery(useRecommendStudyQueryObject());
 
-  const { isOpen, open, close } = useBooleanState();
+  const { mutate: joinRecommendMutate } = useJoinRecommendMutation();
   const [currentIndex, setCurrentIndex] = useState(0);
-  const groupInfo = MOCK_RECOMMEND_STUDIES[currentIndex];
+  const { isOpen, open, close } = useBooleanState();
+
+  const groupInfo = groupInfos?.[currentIndex];
+
+  const handleConfirm = () => {
+    joinRecommendMutate(groupInfo!.id);
+    close();
+  };
 
   useEffect(() => {
-    if (isOpen || MOCK_RECOMMEND_STUDIES.length <= 1) {
-      return;
-    }
+    if (isOpen || groupInfos.length <= 1) return;
 
     const intervalId = setInterval(() => {
-      setCurrentIndex(
-        (prevIndex) => (prevIndex + 1) % MOCK_RECOMMEND_STUDIES.length,
-      );
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % groupInfos.length);
     }, ROTATION_INTERVAL_MS);
 
     return () => clearInterval(intervalId);
-  }, [isOpen, MOCK_RECOMMEND_STUDIES]);
+  }, [isOpen, groupInfos]);
 
-  const handleClose = () => {
-    close();
-  };
-
-  const handleConfirm = () => {
-    close();
-  };
+  if (!groupInfo) {
+    return null;
+  }
 
   return (
     <section
@@ -91,7 +94,7 @@ const RecommendStudySection = () => {
             tagVariant="recentSignups"
             onClick={open}
           />
-          <GroupActionModal isOpen={isOpen} onClose={handleClose}>
+          <GroupActionModal isOpen={isOpen} onClose={close}>
             <GroupActionModal.Info groupInfo={groupInfo} />
             <GroupActionModal.Prompt
               variant="recommend"
@@ -99,7 +102,7 @@ const RecommendStudySection = () => {
             />
             <GroupActionModal.Actions
               onConfirm={handleConfirm}
-              onReject={handleClose}
+              onReject={close}
               confirmText="신청하기"
               rejectText="취소하기"
             />
@@ -109,46 +112,5 @@ const RecommendStudySection = () => {
     </section>
   );
 };
-
-const MOCK_RECOMMEND_STUDIES: GroupResponse[] = [
-  {
-    id: 1,
-    name: "알코칠",
-    introduction:
-      "BE Developer로 성장하고 싶은 숭실대학교 학생들의 알고리즘 스터디",
-    groupImage: "",
-    endDate: "2023-12-31",
-    ownerNickname: "홍길동",
-    startDate: "2023-01-01",
-    role: "PARTICIPANT",
-    isVisible: true,
-    isBookmarked: true,
-  },
-  {
-    id: 2,
-    name: "코칠마",
-    introduction: "FE Developer로 성장하고 싶은 주니어들의 스터디입니다.",
-    groupImage: "",
-    endDate: "2023-12-12",
-    ownerNickname: "홍서동",
-    startDate: "2023-05-01",
-    role: "PARTICIPANT",
-    isVisible: true,
-    isBookmarked: true,
-  },
-  {
-    id: 3,
-    name: "CS 마스터",
-    introduction: "컴퓨터 과학 기초를 탄탄히 다지고 싶은 분들을 위한 스터디",
-    groupImage: "",
-    endDate: "2024-06-30",
-    ownerNickname: "김철수",
-    startDate: "2024-01-01",
-    role: "OWNER",
-    isVisible: true,
-    isBookmarked: false,
-  },
-];
-const ROTATION_INTERVAL_MS = 5000;
 
 export default RecommendStudySection;
