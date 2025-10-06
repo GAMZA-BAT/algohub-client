a0fe292c3d1b3b7543cd7631e8c074d69c2be641
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-10-06 18:06:07 +0900

    feat: 가입 승인 모달 수정

--- apps/client/src/app/group/[groupId]/setting/components/JoinRequestList/ApprovalCard/index.tsx
+++ apps/client/src/app/group/[groupId]/setting/components/JoinRequestList/ApprovalCard/index.tsx
@@ -2,8 +2,6 @@
 
 import Avatar from "@/common/component/Avatar";
 import Button from "@/common/component/Button";
-import { useBooleanState } from "@/common/hook/useBooleanState";
-import GroupActionModal from "@/shared/component/GroupActionModal";
 import { useId } from "react";
 import {
   actionButtonStyle,
@@ -11,8 +9,6 @@ import {
   cardStyle,
   descriptionWrapper,
   iconStyle,
-  modalStyle,
-  modalTriggerButtonStyle,
   nameStyle,
   textStyle,
 } from "./index.css";
@@ -23,12 +19,7 @@ type ApprovalCardProps = {
   avatarUrl: string;
 };
 
-export const ApprovalCard = ({
-  name,
-  groupName,
-  avatarUrl,
-}: ApprovalCardProps) => {
-  const { isOpen, open, close } = useBooleanState();
+export const ApprovalCard = ({ name, avatarUrl }: ApprovalCardProps) => {
   const nameId = useId();
 
   const handleApprove = (e: React.MouseEvent) => {
@@ -43,30 +34,20 @@ export const ApprovalCard = ({
     close();
   };
 
-  const handleOpenModal = () => {
-    open();
-  };
-
   return (
     <article className={cardStyle} aria-labelledby={nameId}>
-      <button
-        type="button"
-        className={modalTriggerButtonStyle}
-        onClick={handleOpenModal}
-        aria-label={`${name}님의 스터디 가입 요청 상세 보기`}
-      >
-        <div className={descriptionWrapper}>
-          <Avatar
-            className={iconStyle}
-            src={avatarUrl}
-            alt={`${name}의 프로필 사진`}
-          />
-          <p id={nameId} className={textStyle}>
-            <span className={nameStyle}>{name}</span>
-            님의 스터디 가입 요청
-          </p>
-        </div>
-      </button>
+      <div className={descriptionWrapper}>
+        <Avatar
+          className={iconStyle}
+          src={avatarUrl}
+          alt={`${name}의 프로필 사진`}
+        />
+        <p id={nameId} className={textStyle}>
+          <span className={nameStyle}>{name}</span>
+          님의 스터디 가입 요청
+        </p>
+      </div>
+
       <div className={actionWrapperStyle}>
         <Button
           size="small"
@@ -87,20 +68,6 @@ export const ApprovalCard = ({
           거절하기
         </Button>
       </div>
-      <GroupActionModal className={modalStyle} isOpen={isOpen} onClose={close}>
-        <GroupActionModal.Applicant nickname={name} profileImage={avatarUrl} />
-        <GroupActionModal.Prompt
-          variant="applicant"
-          applicantName={name}
-          groupName={groupName}
-        />
-        <GroupActionModal.Actions
-          onConfirm={handleApprove}
-          onReject={handleReject}
-          confirmText="승인하기"
-          rejectText="거절하기"
-        />
-      </GroupActionModal>
     </article>
   );
 };
