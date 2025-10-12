3bea43140da56f4d67fc40bda6c54221eb1ea9b0
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-10-12 20:17:05 +0900

    style: 테이블 외부 요소가 잘리던 원인 제거 (#458)

--- apps/client/src/shared/component/Table/TableElements/index.css.ts
+++ apps/client/src/shared/component/Table/TableElements/index.css.ts
@@ -3,7 +3,6 @@ import { style } from "@vanilla-extract/css";
 import { recipe } from "@vanilla-extract/recipes";
 
 export const wrapperStyle = style({
-  position: "relative",
   display: "flex",
   alignItems: "flex-start",
   justifyContent: "center",
