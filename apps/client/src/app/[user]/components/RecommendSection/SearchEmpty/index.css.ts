52d68531d1e74a4828061691865cf0914165f412
rtttr1 <bobo3766@naver.com>
2025-10-13 20:22:44 +0900

    fix: empty 이미지 기존 파일로 대체

--- apps/client/src/app/[user]/components/RecommendSection/SearchEmpty/index.css.ts
+++ apps/client/src/app/[user]/components/RecommendSection/SearchEmpty/index.css.ts
@@ -2,23 +2,16 @@ import { theme } from "@/styles/themes.css";
 import { style } from "@vanilla-extract/css";
 
 export const emptyWrapper = style({
-  position: "relative",
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
+  gap: "3rem",
 
   width: "100%",
 });
 
 export const emptyGuideStyle = style({
-  position: "absolute",
-  top: "70%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-
   ...theme.font.Body2_R_14,
   color: theme.color.mg4,
-
-  zIndex: 1,
 });
--- apps/client/src/app/[user]/components/RecommendSection/SearchEmpty/index.tsx
+++ apps/client/src/app/[user]/components/RecommendSection/SearchEmpty/index.tsx
@@ -1,16 +1,10 @@
-import imgEmpty from "@/asset/img/img_empty.png";
-import Image from "next/image";
+import IcnEmpty from "@/asset/svg/icn_empty.svg";
 import { emptyGuideStyle, emptyWrapper } from "./index.css";
 
 const SearchEmpty = () => {
   return (
     <div className={emptyWrapper}>
-      <Image
-        src={imgEmpty}
-        alt="검색 결과가 없을 때 이미지"
-        width={369}
-        height={192}
-      />
+      <IcnEmpty width={98} height={51.7} />
       <p className={emptyGuideStyle}>검색 결과가 없습니다.</p>
     </div>
   );
