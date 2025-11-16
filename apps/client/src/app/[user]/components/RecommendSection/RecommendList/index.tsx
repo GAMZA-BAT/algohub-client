22e506857af13b0c0fb9ca7015f65ecd61dc8eeb
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-11-16 00:52:35 +0000

    fix: 스터디 검색 결과 중 추천 스터디의 경우 태그 표시

--- apps/client/src/app/[user]/components/RecommendSection/RecommendList/index.tsx
+++ apps/client/src/app/[user]/components/RecommendSection/RecommendList/index.tsx
@@ -2,8 +2,10 @@ import { studyListWrapper } from "@/app/[user]/components/RecommendSection/Recom
 import SearchEmpty from "@/app/[user]/components/RecommendSection/SearchEmpty";
 import { useJoinRecommendMutation } from "@/app/api/groups/mutation";
 import type { Study } from "@/app/api/groups/type";
+import { useRecommendStudyQueryObject } from "@/app/api/users/query";
 import { useBooleanState } from "@/common/hook/useBooleanState";
 import GroupActionModal from "@/shared/component/GroupActionModal";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import CardButton from "../../CardButton";
 
@@ -14,6 +16,12 @@ type RecommendListProps = {
 const RecommendList = ({ studyList }: RecommendListProps) => {
   const { isOpen, open, close } = useBooleanState();
   const [groupInfo, setGroupInfo] = useState<Study>(studyList[0]);
+  const { data: recommendationItems } = useQuery({
+    ...useRecommendStudyQueryObject(),
+    select(data) {
+      return Object.values(data).map((item) => item.studyGroup);
+    },
+  });
   const { mutate: joinRecommendMutate } = useJoinRecommendMutation();
 
   const handleOpen = (study: Study) => {
@@ -34,11 +42,20 @@ const RecommendList = ({ studyList }: RecommendListProps) => {
   return (
     <>
       <ul className={studyListWrapper} aria-label="검색된 스터디 목록">
-        {studyList.map((study) => (
-          <li key={study.id}>
-            <CardButton groupInfo={study} onClick={() => handleOpen(study)} />
-          </li>
-        ))}
+        {studyList.map((study) => {
+          const tagVariant = recommendationItems?.find(
+            ({ id }) => id === study.id,
+          )?.tags?.[0];
+          return (
+            <li key={study.id}>
+              <CardButton
+                groupInfo={study}
+                tagVariant={tagVariant}
+                onClick={() => handleOpen(study)}
+              />
+            </li>
+          );
+        })}
       </ul>
       <GroupActionModal isOpen={isOpen} onClose={close}>
         <GroupActionModal.Info groupInfo={groupInfo} />
