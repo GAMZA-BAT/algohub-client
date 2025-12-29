3584ee3f4ece1e15a23ff3365702dbc1ad7bbe62
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2025-12-29 14:28:32 +0000

    chore: format 수정

--- apps/client/src/app/group/[groupId]/problem-list/page.tsx
+++ apps/client/src/app/group/[groupId]/problem-list/page.tsx
@@ -55,7 +55,7 @@ const ProblemListPage = ({
         size: 3,
         unsolvedOnly: isUnsolvedOnlyChecked,
       }),
-      searchParam: "inProgress"
+    searchParam: "inProgress",
   });
   const inProgressList = inProgressData?.content;
 
@@ -72,7 +72,7 @@ const ProblemListPage = ({
         page,
         size: 3,
       }),
-      searchParam: "expired"
+    searchParam: "expired",
   });
   const expiredList = expiredData?.content;
 
